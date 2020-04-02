c02c5791a7e674262362103fcf43d3bf13dfd96a
STIV3NS <jacekbizub@gmail.com>
2020-04-02 17:33:12 +0200

    describe getArguments() return values

--- src/phrase_indexer.go
+++ src/phrase_indexer.go
@@ -8,18 +8,15 @@ import (
 )
 
 func main() {
-	thread, start, end, limit, exclude := getArguments()
+	thread, exclude, start, end, limit := getArguments()
 
 	fmt.Printf("%s [ %u - %u ]\n", thread, start, end)
 	fmt.Printf("Limit to %u, exclude: %s\n", limit, exclude)
 }
 
-func getArguments() (string, uint, uint, uint, string) {
+func getArguments() (thread, exclude string, start, end, limit uint) {
 	const sREQUIRED  = ""
 	const iREQUIRED = 0
-
-	var thread, exclude string
-	var start, end, limit uint
 
 	flag.StringVar(&thread, "thread", sREQUIRED, "[REQUIRED] URL to thread that is meant to be indexed")
 	flag.UintVar(&start, "start", 1, "[OPTIONAL] Page number on which to start indexing")
@@ -35,5 +32,5 @@ func getArguments() (string, uint, uint, uint, string) {
 		os.Exit(1)
 	}
 
-	return thread, start, end, limit, exclude
+	return
 }
