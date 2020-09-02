de8ff5a7cfe1f14ef8674753db43cbc388f66d62
STIV3NS <jacekbizub@gmail.com>
2020-09-02 17:45:14 +0200

    Extract utils to separate file

--- Makefile
+++ Makefile
@@ -2,9 +2,11 @@
 GOCMD=go
 GOBUILD=$(GOCMD) build
 GOCLEAN=$(GOCMD) clean
+GOGET=$(GOCMD) get
 
 all: build
 build:
+	$(GOGET) github.com/PuerkitoBio/goquery
 	$(GOBUILD) src/*
 clean:
 	$(GOCLEAN)
--- src/phrase_indexer.go
+++ src/phrase_indexer.go
@@ -3,16 +3,14 @@ package main
 import (
     "flag"
     "fmt"
-    "github.com/PuerkitoBio/goquery"
     "io/ioutil"
     "log"
     "math"
-    "net/http"
     "os"
-    "regexp"
     "sort"
     "strings"
     "sync"
+    "github.com/PuerkitoBio/goquery"
 )
 
 type phraseCnt struct {
@@ -100,7 +98,7 @@ func spawnWorkers(selector string, howMany uint, collector chan<- *map[string]ui
 
 func initJobs(jobs chan<- string, threadURL string, start uint, end uint) {
     for i := start; i <= end; i++ {
-        jobs <- threadURL + fmt.Sprintf("%v", i)
+        jobs <- fmt.Sprintf("%v%v", threadURL, i)
     }
     close(jobs)
 }
@@ -136,59 +134,6 @@ func worker(selector string, jobs <-chan string, collector chan<- *map[string]ui
     }
 }
 
-func getResponse(site string) *http.Response {
-    resp, err := http.Get(site)
-    
-    if err != nil {
-        panic(err)
-    }
-    if resp.StatusCode != 200 {
-        panic(fmt.Sprintf("Response from %v has status code %v. Aborting.", site, resp.StatusCode))
-    }
-    
-    return resp
-}
-
-func getHtml(resp *http.Response) *goquery.Document {
-    doc, err := goquery.NewDocumentFromReader(resp.Body)
-    if err != nil {
-        panic(err)
-    }
-    
-    return doc
-}
-
-func normalize(text *string) {
-    nonWord, _ := regexp.Compile("[0-9`~!@#$%^?&*()_+-=\\[\\]{}|'\";:/.,><]")
-    *text = nonWord.ReplaceAllLiteralString(*text, "")
-    *text = strings.ToLower(*text)
-    replacePolishDiacritics(text)
-}
-
-func replacePolishDiacritics(text *string) {
-    a, _ := regexp.Compile("ą")
-    c, _ := regexp.Compile("ć")
-    e, _ := regexp.Compile("ę")
-    l, _ := regexp.Compile("ł")
-    o, _ := regexp.Compile("ó")
-    s, _ := regexp.Compile("ś")
-    z, _ := regexp.Compile("[żź]")
-    
-    replacements := map[*regexp.Regexp]string{
-        a : "a",
-        c : "c",
-        e : "e",
-        l : "l",
-        o : "o",
-        s : "s",
-        z : "z",
-    }
-    
-    for regexptr, repl := range replacements {
-        *text = regexptr.ReplaceAllLiteralString(*text, repl)
-    }
-}
-
 func sortByPhraseCount(phraseCounts *map[string]uint32) *[]phraseCnt {
     ranking := make([]phraseCnt, len(*phraseCounts))
     
@@ -207,17 +152,6 @@ func sortByPhraseCount(phraseCounts *map[string]uint32) *[]phraseCnt {
     return &ranking
 }
 
-func filter(src *[]phraseCnt, predicate func(phraseCnt) bool) *[]phraseCnt {
-    filtered := make([]phraseCnt, 0)
-    
-    for _, elem := range *src {
-        if predicate(elem) {
-            filtered = append(filtered, elem)
-        }
-    }
-    
-    return &filtered
-}
 
 func applyExclusions(ranking **[]phraseCnt, exclude string) {
     exclusions := getExclusions(exclude)
